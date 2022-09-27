5626193ce9dded0a5855c9562bd195a7cd41cbb5
David Juanwu Lu <juanwu_lu@berkeley.edu>
2022-09-26 17:24:43 -0700

    [Sep 26] Fix bugs in scripts

--- hw2/cs285/scripts/run_half_cheetah_search.sh
+++ hw2/cs285/scripts/run_half_cheetah_search.sh
@@ -1,10 +1,10 @@
 echo "Search for optimal batch_size and learning rate for HalfCheetah.";
-for batch in 10000 30000 50000
+for BATCh in 10000 30000 50000
 do
-    for lr in 0.005 0.01 0.02
+    for LR in 0.005 0.01 0.02
     do
-        echo "Now running on batch_size=${batch}, learning_rate=${lr}.";
-        name = "q4_search_b${batch}_lr${lr}_rtg_nnbaseline";
-        python $1 --env_name HalfCheetah-v4 --ep_len 150 --discount 0.95 -n 100 -l 2 -s 32 -b $batch -lr $lr -rtg --nn_baseline --exp_name $name;
+        echo "Now running on batch_size=${BATCH}, learning_rate=${LR}.";
+        NAME="q4_search_b${BATCH}_lr${LR}_rtg_nnbaseline";
+        python $1 --env_name HalfCheetah-v4 --ep_len 150 --discount 0.95 -n 100 -l 2 -s 32 -b $BATCH -lr $LR -rtg --nn_baseline --exp_name $NAME;
     done
 done
