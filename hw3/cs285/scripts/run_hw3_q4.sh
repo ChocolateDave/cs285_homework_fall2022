143587e31644b5fccc26a61da5b5ab694b51d74e
David Juanwu Lu <juanwu_lu@berkeley.edu>
2022-10-08 02:53:08 -0700

    [Oct 8] Update q2 and q4

--- hw3/cs285/scripts/run_hw3_q4.sh
+++ hw3/cs285/scripts/run_hw3_q4.sh
@@ -1,6 +1,6 @@
 echo "Running Homework 3 Question 4..."
-python $1 --env_name CartPole-v0 -n 100 -b 1000 --exp_name q4_ac_1_1 -ntu 1 -ngsptu 1
-python $1 --env_name CartPole-v0 -n 100 -b 1000 --exp_name q4_ac_100_1 -ntu 100 -ngsptu 1
-python $1 --env_name CartPole-v0 -n 100 -b 1000 --exp_name q4_ac_1_100 -ntu 1 -ngsptu 100
-python $1 --env_name CartPole-v0 -n 100 -b 1000 --exp_name q4_ac_10_10 -ntu 10 -ngsptu 10
+python $1 --env_name CartPole-v0 -n 100 -b 1000 --exp_name q4_ac_1_1 -ntu 1 -ngsptu 1 -gpu_id $2
+python $1 --env_name CartPole-v0 -n 100 -b 1000 --exp_name q4_ac_100_1 -ntu 100 -ngsptu 1 -gpu_id $2
+python $1 --env_name CartPole-v0 -n 100 -b 1000 --exp_name q4_ac_1_100 -ntu 1 -ngsptu 100 -gpu_id $2
+python $1 --env_name CartPole-v0 -n 100 -b 1000 --exp_name q4_ac_10_10 -ntu 10 -ngsptu 10 -gpu_id $2
 echo "Done!"
